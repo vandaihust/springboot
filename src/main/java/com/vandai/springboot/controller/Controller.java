c37502dbbc93bc1d683a010504dec96e88a3b32f
cris <>
2024-06-16 09:28:53 +0700

    f

--- src/main/java/com/vandai/springboot/controller/Controller.java
+++ src/main/java/com/vandai/springboot/controller/Controller.java
@@ -1,12 +1,20 @@
 package com.vandai.springboot.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
     @GetMapping
-    public String test(){
+    public String test() {
         return "test";
     }
+
+    @GetMapping("fake")
+    public ResponseEntity<?> fake() {
+        return new ResponseEntity<>("fake", HttpStatus.OK);
+    }
+
 }
